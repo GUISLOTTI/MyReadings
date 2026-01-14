bab8965c7c4c64805557140240f5131043a34a78
Gabriel Guislotti <gabrielguislotti@hotmail.com>
2026-01-13 21:31:53 -0300

    Created Web Project and some classes

---
+++ src/MyReadings.Web/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyReadings.Web.Controllers
+{
+    public class AccountController : ControllerBase
+    {
+        public AccountController() { }
+
+        #region GET
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+        //[HttpGet]
+        //[AllowAnonymous]
+        //public ActionResult Login(string returnUrl)
+        //{
+        //    //if (User.Identity.IsAuthenticated)
+        //    //{
+        //    //    return RedirectToAction("Index", "Dashboard");
+        //    //}
+        //    //ViewBag.ReturnUrl = returnUrl;
+        //    //return View();
+        //} 
+        #endregion
+    }
+}
---
+++ src/MyReadings.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyReadings.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
